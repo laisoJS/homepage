5635447f7390c1d75aa39e6bb91d710e97fb071d
laiso <aloisleroy2016@gmail.com>
2024-03-14 11:23:34 +0100

    Add config link for easier

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,14 @@
-username = "laisoJS";
+const username = "laisoJS",             // Username
+      userIcon = "../img/pdp.png",      // user icon next to the title
+      pageIcon = "../img/icon.png";     // Document icon
 
-const user = document.getElementById("username");
+// Get elements
+const user = document.getElementById("username"),
+      icon = document.getElementById("pdp"),
+      documentIcon = document.getElementById("icon");
+
+icon.src = userIcon;                    // Change the icon depending on the user icon
+documentIcon.href = pageIcon;           // Change page icon depending on the page icon
+
 user.innerHTML = `Hello ${username}`;   // Change the title depending of the username
 document.title = `Welcome ${username}`; // change the page title depending of the username
